61f2396467b7f2a72248a9bb5f7072bb3e257d21
George M <gsmaroun@gmail.com>
2024-04-25 16:17:59 -0400

    Fix some buggies

--- lib/wagmi-provider.tsx
+++ lib/wagmi-provider.tsx
@@ -4,7 +4,9 @@ import React, { ReactNode } from "react";
 import { createWeb3Modal } from "@web3modal/wagmi/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { State, WagmiProvider } from "wagmi";
-import { config, projectId } from "./config";
+import { config } from "./config";
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "";
  
 if (!projectId) throw new Error("Project ID is not defined");
  
