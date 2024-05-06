599a047e66b2953f8321b3d706241051b401d4cf
George M <gsmaroun@gmail.com>
2024-05-06 15:05:40 -0400

    Add link to download nft

--- src/app/components/Overlay.tsx
+++ src/app/components/Overlay.tsx
@@ -1,8 +1,9 @@
 "use client"
 import { createPortal } from 'react-dom';
-import { SiFarcaster } from "react-icons/si";
+// import { SiFarcaster } from "react-icons/si";
 import { SiOpensea } from "react-icons/si";
 import { FaXTwitter } from "react-icons/fa6";
+import { IoDownloadOutline } from "react-icons/io5";
 // import Image from 'next/image';
 // TODO: Refactor this pasta code
 
@@ -20,6 +21,16 @@ const Overlay = ({ address, nftData, imageData, collectionAddress, onClose }: IO
     // Return null or a fallback component for server-side rendering environments
     return null;
   }
+
+  const handleDownloadImage = () => {
+    // Create a link element
+    const link = document.createElement('a');
+    link.href = imageData; // Set the href to the image data URL
+    link.download = "my_nft.png"; // Set the download attribute with a filename
+    document.body.appendChild(link); // Append the link to the body
+    link.click(); // Trigger the click to download
+    document.body.removeChild(link); // Clean up by removing the link
+  };
   
   return createPortal(
     <div className="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center z-50 p-4 " onClick={onClose}>
@@ -35,10 +46,10 @@ const Overlay = ({ address, nftData, imageData, collectionAddress, onClose }: IO
             <FaXTwitter/> Share on X
           </button>
           <button 
-            onClick={() => window.open(`https://warpcast.com/`, '_blank')}
+            onClick={handleDownloadImage}
             className='lg:w-6/12 w-full h-10 pl-4 pr-4 border border-gray-200 rounded-full text-sm font-semibold text-gray-700 hover:bg-gray-100 hover:border-gray-300 flex flex-row items-center justify-center gap-1 mt-4'
           >
-            <SiFarcaster/> Share on Farcaster
+            <IoDownloadOutline /> Save Image
           </button>
           
         </div>
