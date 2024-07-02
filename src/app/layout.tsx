3808021d4f381c79eb1e52716a99028d6751a6c1
George <93730150+george-maroun@users.noreply.github.com>
2024-07-02 14:32:45 -0400

    add cookie3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import WagmiProviderComp from "../../lib/wagmi-provider";
 import { config } from "../../lib/config";
 import { cookieToInitialState } from "wagmi";
 import { headers } from "next/headers";
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,17 @@ export default function RootLayout({
   return (
     <WagmiProviderComp initialState={initialState}>
         <html lang="en">
+          <head>
+            <Script
+              src="https://cdn.cookie3.co/scripts/analytics/0.11.4/cookie3.analytics.min.js"
+              integrity="sha384-lzDmDdr/zEhMdlE+N04MgISCyL3RIWNCb9LjsrQeEFi8Gy5CKXIRI+u58ZV+ybYz"
+              crossOrigin="anonymous"
+              async
+              strategy="lazyOnload"
+              data-site-id="1271"
+              data-chain-tracking-enabled="true"
+            />
+        </head>
           <body className={inter.className}>
             {children}
           </body>
